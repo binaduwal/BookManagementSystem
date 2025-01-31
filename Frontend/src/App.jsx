a58e4647a874259b5ae559220449690855c78f51
Bina <duwalbina2@gmail.com>
2025-01-31 07:39:30 +0545

    added useState

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,22 @@
-import Button from "./Button"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, RouterProvider} from "react-router-dom"
+
 import Home from "./Home"
+import router from "./Routes"
+import { useState } from "react"
 
 function App() {
+const [count,setcount]= useState(0)
+const [name,setName]=useState("Bina")
+
+
   return (
     <>
-    {/* <Button bhitrakokura="Register"/>
-    <Button bhitrakokura="Login"/>
-    <Button bhitrakokura="SignUp"/>
-     */}
-  <BrowserRouter>
-  <Routes>
-
-    <Route path="/" element={<Home/>}/>
-    <Route path="/login" element={<h1>login</h1>}/>
-    <Route path="/signup" element={<h1>signup</h1>}/>
-  </Routes>
-  </BrowserRouter>
-
+    <h1>{count}</h1>
+    <h2>{name}</h2>
+    <button onClick={()=>setcount(count +1)}>+</button>
+    <button onClick={()=>setName("Beena")}>Change Name</button>
+  <RouterProvider router={router}/>
     </>
- 
-
   )
 }
 
---
+++ Frontend/src/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {createBrowserRouter} from 'react-router-dom'
+const router =createBrowserRouter([
+{
+    path:"/",
+    element:<h1>Hello this is home page</h1>
+}
+])
+
+export default router
